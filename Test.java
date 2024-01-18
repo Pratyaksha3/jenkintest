757ec6c1899b272611d2099090b71e94b0ebad85
Pratyaksha <pj3@gmail.com>
2024-01-18 06:17:47 +0000

    Initial commit done

---
+++ Test.java
@@ -0,0 +1,36 @@
+class Test
+{
+public static void main(String args[])
+{
+int a = 10;
+int b = 15;
+int result;
+result = a+b;
+System.out.println("Result of addition: "+ result);
+System.out.println("Test class runs successfully with correct result");
+}
+}
+
+
+
+~                                                                                                                                      
+
+~                                                                                                                                      
+
+~                                                                                                                                      
+
+~                                                                                                                                      
+
+~                                                                                                                                      
+
+~                                                                                                                                      
+
+~                                                                                                                                      
+
+~                                                                                                                                      
+
+~                                                                                                                                      
+
+~                                                                                                                                   ~                                                                                                               
+
+
